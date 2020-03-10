3f90af92c38ca8b2fc123c650ff66821b34decad
Yohann Marchand <marchandyohannnnn@gmail.com>
2020-03-10 17:29:54 +0100

    add presentation section

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+const Parallax = require('parallax-js');
+
+var sceneTitle = document.getElementById('scenetitle');
+var parallaxInstance = new Parallax(sceneTitle);
+
+var scenePresentation = document.getElementById('scenepresentation');
+var parallaxInstance = new Parallax(scenePresentation);
