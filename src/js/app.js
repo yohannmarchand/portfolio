e777bc2612f5e04a39d50c8c83f90a627af07951
Yohann Marchand <marchandyohannnnn@gmail.com>
2020-03-11 15:55:52 +0100

    add realisation part

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,29 @@ var parallaxInstance = new Parallax(sceneTitle);
 
 var scenePresentation = document.getElementById('scenepresentation');
 var parallaxInstance = new Parallax(scenePresentation);
+
+
+$(".open").click(function () {
+
+
+  container = $(this).parent().parent();
+  container.parent().children().css('display', 'none');
+  container.addClass('show');
+
+  $('.close').show()
+  $(this).hide()
+
+
+})
+
+$('.close').click(function () {
+
+  container = $(this).parent().parent();
+  container.parent().children().css('display', 'block');
+  container.removeClass('show');
+  $('.close').hide();
+  $('.open').show()
+
+
+
+})
