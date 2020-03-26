e28e890a4cbbb0c85f71ecceb31364c64ec1cac2
Yohann Marchand <yohannmarchand@MacBook-Pro-de-Yohann.local>
2020-03-26 19:06:12 +0100

    add footer

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,4 @@
-const Parallax = require('parallax-js');
 
-var sceneTitle = document.getElementById('scenetitle');
-var parallaxInstance = new Parallax(sceneTitle);
-
-var scenePresentation = document.getElementById('scenepresentation');
-var parallaxInstance = new Parallax(scenePresentation);
 
 
 $(".open").click(function () {
